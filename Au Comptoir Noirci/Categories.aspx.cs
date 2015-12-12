573743ed0ee9b5b131ccc86aaab9945e5d7bb592
Lionel <lionel.comarin@viacesi.fr>
2015-12-12 16:09:50 +0100

    Détail_Article.aspx --Changement du nom Détail_Article en Detail_Article --Ajout de la description dans l'image --Modification des Balises prix et Description pour un id de description

--- Au Comptoir Noirci/Categories.aspx.cs
+++ Au Comptoir Noirci/Categories.aspx.cs
@@ -12,30 +12,30 @@ namespace Au_Comptoir_Noirci
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            string contenu = "";
-            string connectionString = .Properties.Settings.Default.ConnectionString;
-            //
-            // In a using statement, acquire the SqlConnection as a resource.
-            //
-            using (SqlConnection con = new SqlConnection(la_reserveConnectionString))
-            {
-                //
-                // Open the SqlConnection.
-                //
-                con.Open();
-                //
-                // The following code uses an SqlCommand based on the SqlConnection.
-                //
-                SqlCommand command = new SqlCommand("SELECT id, nom from categories", con);
-                SqlDataReader reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    contenu += "<div class=\"categorie\"><h2>" + reader.GetString(1) + "</h2>";
-                    command = new SqlCommand("SELECT id, titre, substring(contenu, 0, 250), statut, prix, note from article", con);
-                    contenu += "</div>";
-                }
-                con.Close();
-            }
+            //string contenu = "";
+            //string connectionString = .Properties.Settings.Default.ConnectionString;
+            ////
+            //// In a using statement, acquire the SqlConnection as a resource.
+            ////
+            //using (SqlConnection con = new SqlConnection(la_reserveConnectionString))
+            //{
+            //    //
+            //    // Open the SqlConnection.
+            //    //
+            //    con.Open();
+            //    //
+            //    // The following code uses an SqlCommand based on the SqlConnection.
+            //    //
+            //    SqlCommand command = new SqlCommand("SELECT id, nom from categories", con);
+            //    SqlDataReader reader = command.ExecuteReader();
+            //    while (reader.Read())
+            //    {
+            //        contenu += "<div class=\"categorie\"><h2>" + reader.GetString(1) + "</h2>";
+            //        command = new SqlCommand("SELECT id, titre, substring(contenu, 0, 250), statut, prix, note from article", con);
+            //        contenu += "</div>";
+            //    }
+            //    con.Close();
+            //}
         }
     }
 }
---
+++ Au Comptoir Noirci/Detail_Article.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Title="Détail d'article" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Detail_Article.aspx.cs" Inherits="Au_Comptoir_Noirci.Détail_Article" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container">
+        <div class="col-md-9">
+            <h2 id="titre_article"> Article présenté</h2>
+            <div id="photo_article"><img id="presentation_article" src="content/img/target-512.png"/></div>
+            <p id="pas_de_photos"><span id="photo_texte">Il n'y a actuellement aucune gravure pour cet article.</span></p>
+            <p id="prix" ><span class="sous-titre">Prix: </span><span id="prix_texte">Prix à débattre</span></p>
+            <p id="description"><span class="sous-titre">Description:</span><br /><span id="description_texte">Il n'y a actuellement aucune description</span></p>
+            <a class="menuliste list-group-item" href="Default.aspx">Retourner à l'accueil</a>
+        </div>
+    </div>
+</asp:Content>
